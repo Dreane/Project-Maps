53e16cc1b005d7be87087a8d3cb145514e5816c0
Dreane <85702463+Dreane@users.noreply.github.com>
2023-02-07 21:16:40 +0500

    task 2

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import sys
 import os
 
 
-def load_map():
-    map_request = "http://static-maps.yandex.ru/1.x/?ll=37.621094,55.753605&spn=0.05,0.05&l=map"
+def load_map(scale):
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll=37.621094,55.753605&spn={scale},{scale}&l=map"
     response = requests.get(map_request)
-
+    print(scale)
     if not response:
         print("Ошибка выполнения запроса:")
         print(map_request)
@@ -17,18 +17,28 @@ def load_map():
     map_file = "map.png"
     with open(map_file, "wb") as file:
         file.write(response.content)
-    return map_file
+    return map_file, scale
 
 
 def main():
     pygame.init()
     running = True
     screen = pygame.display.set_mode((600, 450))
+    scale = 1
     while running:
+        map_file, scale = load_map(scale)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        map_file = load_map()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    if 0.01 <= (scale + 8) <= 50:
+                        map_file, scale = load_map(scale + 8)
+
+                elif event.key == pygame.K_UP:
+                    if 0.01 <= (scale - 8) <= 50:
+                        map_file, scale = load_map(scale - 8)
+
         screen.blit(pygame.image.load(map_file), (0, 0))
         pygame.display.flip()
     os.remove(map_file)
